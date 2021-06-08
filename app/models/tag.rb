ea85cf87c870b6acbbc81b6680cd9329dcacfb40
shunsaku0621 <hamagi.0621@icloud.com>
2021-06-08 18:59:10 +0900

    アソシエーションとformオブジェクトパターン

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,9 @@
 class Tag < ApplicationRecord
+  has_many :post_tag_relations
+  has_many :posts, through: :post_tag_relations
+
+  # 一意性の制約はモデル単位で設ける必要がある
+  validates :name, uniqueness: true
+
+  # validates :name, presence: true
 end
