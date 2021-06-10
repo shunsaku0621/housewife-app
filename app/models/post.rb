15860471f195b99bc4452cf6a6c8a4ccce6ccbd7
shunsaku0621 <hamagi.0621@icloud.com>
2021-06-10 17:45:53 +0900

    削除機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
   
-  has_many :post_tag_relations
+  has_many :post_tag_relations, dependent: :destroy
   has_many :tags, through: :post_tag_relations
   has_one_attached :image
 
