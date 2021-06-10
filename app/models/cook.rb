3d02ee016759c0609a69080f1a9925a6be77130d
shunsaku0621 <hamagi.0621@icloud.com>
2021-06-10 20:32:39 +0900

    コントローラー　ルーティング

--- app/models/cook.rb
+++ app/models/cook.rb
@@ -1,2 +1,16 @@
 class Cook < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :name, length: {maximum: 30}
+  end
+  validates :description, length: {maximum: 140}
+  validates :point, length: {maximum: 50}
+  validates :appearance,
+            :numericality => {
+              :only_interger => true,
+              :greater_than_or_equal_to => 1,
+              :less_than_or_equal_to => 5
+            },
+            allow_nil: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "tops#index"
   resources :tops, only: [:index]
   resources :posts
+  resources :cooks
 end
