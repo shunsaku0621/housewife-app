6c4042aba72db28a77d31781ad737460ad8886c3
shunsaku0621 <hamagi.0621@icloud.com>
2021-06-10 21:28:14 +0900

    投稿機能の完成

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -7,6 +7,13 @@ class CooksController < ApplicationController
   end
 
   def create
+    @cook = Cook.new(cook_params)
+    if @cook.valid?
+      @cook.save
+      return redirect_to cooks_path
+    else
+      render "new"
+    end
   end
 
 
@@ -15,5 +22,8 @@ class CooksController < ApplicationController
 
 
   private
-    
+  def cook_params
+    params.require(:cook).permit(:name, :description, :portion, :point, :reference, :time, :appearance, :memo).merge(user_id: current_user.id)
+  end
+
 end
