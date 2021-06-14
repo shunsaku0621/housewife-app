5d40e88f9dc244888f961d72b56f3a41121323e6
shunsaku0621 <hamagi.0621@icloud.com>
2021-06-14 15:52:31 +0900

    お気に入り一覧表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tops#index"
-  resources :tops, only: [:index]
+  resources :tops, only: [:index, :show]
   resources :posts
   resources :cooks do 
     resource :favorites, only: [:create, :destroy]
