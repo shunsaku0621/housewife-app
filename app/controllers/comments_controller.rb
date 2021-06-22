37e8eaf9598a898a69b3c0f0be8a47f6fdff05b5
shunsaku0621 <hamagi.0621@icloud.com>
2021-06-22 17:15:11 +0900

    コントローラーの編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(comment_params)
+    redirect_to post_path(comment.post.id)
+  end
+
+
+
+  private
+
+  def comment_params
+    Params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
 end
