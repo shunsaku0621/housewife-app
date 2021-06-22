522fc3c402fbaf4a0da8f16c285cfbc93ddcf764
shunsaku0621 <hamagi.0621@icloud.com>
2021-06-22 18:26:26 +0900

    フラッシュコメント

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
+    flash[:notice] = 'コメントしました!'
     redirect_to post_path(comment.post.id)
   end
 
