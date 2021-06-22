e7090806b108d145b70c2bb1d16908a585f50351
shunsaku0621 <hamagi.0621@icloud.com>
2021-06-22 17:22:31 +0900

    コメントのtext保存

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(comment_params)
+    comment = Comment.create(comment_params)
     redirect_to post_path(comment.post.id)
   end
 
@@ -9,6 +9,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    Params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 end
