45ea6b116358591213834acf381d1349673010de
shunsaku0621 <hamagi.0621@icloud.com>
2021-06-23 14:44:15 +0900

    修正

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,15 +1,17 @@
 class Genre < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '経済' },
-    { id: 3, name: '政治' },
-    { id: 4, name: '地域' },
-    { id: 5, name: '国際' },
-    { id: 6, name: 'IT' },
-    { id: 7, name: 'エンタメ' },
-    { id: 8, name: 'スポーツ' },
-    { id: 9, name: 'グルメ' },
-    { id: 10, name: 'その他' }
+    { id: 2, name: 'なんでも' },
+    { id: 3, name: '相談' },
+    { id: 4, name: 'ニュース' },
+    { id: 5, name: 'エンタメ・芸能人' },
+    { id: 6, name: 'ファション' },
+    { id: 7, name: '美容・健康' },
+    { id: 8, name: '趣味' },
+    { id: 9, name: '旦那・子育て' },
+    { id: 10, name: '料理・家事' },
+    { id: 11, name: 'ダイエット' },
+    { id: 12, name: 'その他' }
   ]
 
 
