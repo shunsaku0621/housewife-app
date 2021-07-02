af6b598a82b33a2b304f0eb806b5abb9105f1f54
shunsaku0621 <hamagi.0621@icloud.com>
2021-07-02 15:18:40 +0900

    値の取得

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -1,1 +1,9 @@
-console.log("読み込み完了");
+if (location.pathname.match("posts/new")){
+  document.addEventListener("DOMContentLoaded", () => {
+    const inputElement = document.getElementById("post_name");
+    inputElement.addEventListener("keyup", ()=>{
+      const keyword = document.getElementById("post_name").value;
+      console.log(keyword);
+    });
+  });
+};
