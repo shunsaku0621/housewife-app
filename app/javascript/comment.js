6bae8bc20a9b36c2ec7dd1417cf0f497590c8984
shunsaku0621 <hamagi.0621@icloud.com>
2021-07-05 18:35:16 +0900

    修正

--- app/javascript/comment.js
+++ app/javascript/comment.js
@@ -1,8 +1,14 @@
-// function post (){
-//   const submit = document.getElementById("post-comment-submit");
-//   submit.addEventListener("click", ()=>{
-//     console.log("発火");
-//   });
-// };
+function post (){
+  const submit = document.getElementById("post-comment-submit");
+  submit.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const form = document.getElementById("comment-form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", `/posts/63/comments`, true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+};
 
-// window.addEventListener('load', post);
+window.addEventListener('load', post);
