1f56a07e17166b4706993e92983a9e60d2f24c71
shunsaku0621 <hamagi.0621@icloud.com>
2021-07-09 20:38:44 +0900

    修正

--- app/javascript/comment.js
+++ app/javascript/comment.js
@@ -1,14 +1,16 @@
-function post (){
-  const submit = document.getElementById("post-comment-submit");
-  submit.addEventListener("click", (e)=>{
-    e.preventDefault();
-    const form = document.getElementById("comment-form");
-    const formData = new FormData(form);
-    const XHR = new XMLHttpRequest();
-    XHR.open("POST", `/posts/63/comments`, true);
-    XHR.responseType = "json";
-    XHR.send(formData);
-  });
-};
+// function post (){
+//   const submit = document.getElementById("post-comment-submit");
+//   submit.addEventListener("click", (e)=>{
+//     e.preventDefault();
+//     const form = document.getElementById("comment-form");
+//     const formData = new FormData(form);
+//     const test = document.getElementById("comment_text");
+//     console.log(test);
+//     const XHR = new XMLHttpRequest();
+//     XHR.open("POST", `/posts/62/comments`, true);
+//     XHR.responseType = "json";
+//     XHR.send(formData);
+//   });
+// };
 
-window.addEventListener('load', post);
+// window.addEventListener('load', post);
