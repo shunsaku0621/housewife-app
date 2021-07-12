e6dc7837e7191243a4e3fd5ac345003571fabc72
shunsaku0621 <hamagi.0621@icloud.com>
2021-07-12 19:24:24 +0900

    likeコントローラーの記述

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
+  def create
+    @like = current_user.likes.create(cook_id: params[:cook_id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @cook = Cook.find(params[:cook_id])
+    @like = current_user.likes.find_by(cook_id: @cook.id)
+    @like.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
 end
