bf20906fbc5b7860f9ef6df1c4cbd6895a12e0dc
shunsaku0621 <hamagi.0621@icloud.com>
2021-07-29 19:45:29 +0900

    バリデーション

--- app/models/getmoney.rb
+++ app/models/getmoney.rb
@@ -2,4 +2,11 @@ class Getmoney < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category2
   belongs_to :user
+
+
+  with_options presence: true do
+    validates :amount
+    validates :start_time
+  end
+  validates :category_id, numericality: { other_than: 1 }
 end
