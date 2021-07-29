052fa6b7af1e7d2b96e4039890180be7cba3e47a
shunsaku0621 <hamagi.0621@icloud.com>
2021-07-29 19:40:50 +0900

    マイグレーションの実行

--- db/migrate/20210729103407_create_getmoneys.rb
+++ db/migrate/20210729103407_create_getmoneys.rb
@@ -1,7 +1,11 @@
 class CreateGetmoneys < ActiveRecord::Migration[6.0]
   def change
     create_table :getmoneys do |t|
-
+      t.integer :amount, null: false
+      t.integer :category_id, null: false
+      t.text :memo
+      t.datetime :start_time, null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
