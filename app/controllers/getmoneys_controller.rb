04a2ddf1d7035dbb83fa0754a19efdf1b4322184
shunsaku0621 <hamagi.0621@icloud.com>
2021-07-30 01:11:53 +0900

    収入登録機能

--- app/controllers/getmoneys_controller.rb
+++ app/controllers/getmoneys_controller.rb
@@ -1,2 +1,21 @@
 class GetmoneysController < ApplicationController
+  def new
+    @getmoney = Getmoney.new
+  end
+
+  def create
+    @getmoney = Getmoney.new(getmoney_params)
+    if @getmoney.save
+      redirect_to incomes_path
+    else 
+      render :new
+    end
+  end
+
+
+
+  def getmoney_params
+    params.require(:getmoney).permit(:amount, :memo, :category_id, :start_time).merge(user_id: current_user.id)
+  end
+
 end
