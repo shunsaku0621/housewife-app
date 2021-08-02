65fa671dbd53d5ed18580be403d175394224c72f
shunsaku0621 <hamagi.0621@icloud.com>
2021-08-02 19:28:39 +0900

    条件下でclass名変更

--- app/javascript/income-color.js
+++ app/javascript/income-color.js
@@ -1,5 +1,13 @@
-function post (){
-  console.log("イベント発火");
+function sum (){
+  const sum = document.getElementById('getmoney-income-sum');
+  const s = sum.textContent;
+  if (s.includes("-")){
+    sum.classList.remove('l');
+    sum.classList.add('a');
+  } else{
+    sum.classList.remove('a');
+    sum.classList.add('l');
+  }
 };
 
-window.addEventListener('load', post);
+window.addEventListener('load', sum);
