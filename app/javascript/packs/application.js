2f9eb3d9b0f24d641aed7655d3bb120b4b0c2cc3
shunsaku0621 <hamagi.0621@icloud.com>
2021-08-02 18:49:26 +0900

    jsファイルの生成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require('jquery')
 require("../tag")
 require("../comment")
+require("../income-color")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
