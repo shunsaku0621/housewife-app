05ac1da71096b20725930c4f5beb64ca097dcc3d
shunsaku0621 <hamagi.0621@icloud.com>
2021-08-15 01:04:15 +0900

    修正

--- app/javascript/cook-review.js
+++ app/javascript/cook-review.js
@@ -3,24 +3,26 @@ function post (){
   const ratingValue = document.getElementById("rating-value");
   const ratingValueDisplay = document.getElementById("rating-value-display");
 
+  let index;
+
+
   for(let i=0; i<stars.length; i++){
-    console.log(stars.length)
     // 星にカーソルが乗ったときに実行する関数を定義
     stars[i].addEventListener("mouseover",function(){
       // stars.lengthの値（5）だけ繰り返し
       // つまり、星カーソルが乗った時、5回繰り返される
       for(let j=0; j<stars.length; j++){
-        console.log(stars.length)
+  
         // まず全て星をくり抜く
-        stars[j].classList.remove("far fa-star");
-        stars[j].classList.add("fa-star-o");
+        stars[j].classList.remove("fa-star-o");
+        stars[j].classList.add("fa-star");
       }
       // そのあと星の数だけ以下の関数が繰り返される。
       for(let j=0; j<=i; j++){
-        console.log(stars.length)
-        stars[j].classList.remove("fa-star-o");
+  
+        stars[j].classList.remove("fas-star");
         // カーソルが乗った星まで星を塗りつぶす
-        stars[j].classList.add("far fa-star");
+        stars[j].classList.add("far-star");
       }
     })
     // クリックされた星の番号+1をratingValueに代入
@@ -35,13 +37,13 @@ function post (){
       // まず5回繰り返す
       for(let j=0; j<stars.length; j++){
         // まず全ての星をくり抜く
-        stars[j].classList.remove("far fa-star");
+        stars[j].classList.remove("fa-star");
         stars[j].classList.add("fa-star-o");
       }
       for(let j=0; j<=index; j++){
         // クリックされている星まで塗りつぶす
         stars[j].classList.remove("fa-star-o");
-        stars[j].classList.add("far fa-star");
+        stars[j].classList.add("fa-star");
       }
     })
   }
