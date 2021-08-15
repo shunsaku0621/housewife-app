4b1c81c13970958a85ff2ff6f9aa130ef1fe20a6
shunsaku0621 <hamagi.0621@icloud.com>
2021-08-15 16:35:14 +0900

    レビュー内容保存

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,21 @@
 class ReviewsController < ApplicationController
+  def index
+  end
+  
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to cook_reviews_path(@review.cook)
+    else
+      render "cooks/show"
+    end
+  end
+
+
+
+  private
+    def review_params
+      params.require(:review).permit(:score, :content, :cook_id)
+    end
 end
