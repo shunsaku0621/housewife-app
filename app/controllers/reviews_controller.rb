61483e8639765f1f790feadb4220eeb128114edb
shunsaku0621 <hamagi.0621@icloud.com>
2021-08-16 13:00:50 +0900

    修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,7 @@ class ReviewsController < ApplicationController
     @cook = Cook.find(params[:cook_id])
     @reviews = @cook.reviews
     avg_score
+    avg_score_percentage
   end
   
   def create
@@ -28,6 +29,14 @@ class ReviewsController < ApplicationController
         @avg_score_cook = @cook.reviews.average(:score).round(1).to_f
       else 
         @avg_score_cook = 0.0
+      end
+    end
+
+    def avg_score_percentage
+      unless @cook.reviews.empty?
+        @avg_score_percentage = @cook.reviews.average(:score).round(1).to_f*100/5
+      else 
+        @avg_score_percentage = 0.0
       end
     end
 end
